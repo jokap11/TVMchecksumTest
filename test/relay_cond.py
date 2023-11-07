5e2e9b6712c84475f31f7b99ecaf98f4f6a36a7c
jokap11 <ge25der@mytum.de>
2023-11-07 12:28:21 +0100

    Updated tvm and played around with passes to check right tensor shape

--- test/relay_cond.py
+++ test/relay_cond.py
@@ -4,6 +4,25 @@ from scipy import special
 import tvm
 import tvm.relay as relay
 
+# def example():
+#     shape = (1, 64, 54, 54)
+#     c_data = np.empty(shape).astype("int8")
+#     c = relay.const(c_data,dtype="int8")
+#     weight = relay.var("weight", shape=(64, 64, 3, 3),dtype="int8")
+#     x = relay.var("x", relay.TensorType((1, 64, 56, 56), "int8"))
+#     conv = relay.nn.conv2d(x, weight)
+#     #weight2 = relay.multiply(weight, relay.const(5, "int8"))
+#     conv2 = relay.nn.conv2d(x, weight)
+#     y = relay.add(c, c)
+#     y = relay.multiply(y, relay.const(2, "int8"))
+#     y = relay.add(conv, y)
+#     z = relay.add(y, c)
+#     z1 = relay.add(y, c)
+#     z2 = relay.add(z, z1)
+#     z2 = relay.exp(z2)
+#     mult_out = relay.Tuple([z2, conv, conv2])
+#     return relay.Function([x, weight], mult_out)
+
 def example():
     shape = (1, 64, 54, 54)
     c_data = np.empty(shape).astype("int8")
@@ -11,17 +30,7 @@ def example():
     weight = relay.var("weight", shape=(64, 64, 3, 3),dtype="int8")
     x = relay.var("x", relay.TensorType((1, 64, 56, 56), "int8"))
     conv = relay.nn.conv2d(x, weight)
-    #weight2 = relay.multiply(weight, relay.const(5, "int8"))
-    conv2 = relay.nn.conv2d(x, weight)
-    y = relay.add(c, c)
-    y = relay.multiply(y, relay.const(2, "int8"))
-    y = relay.add(conv, y)
-    z = relay.add(y, c)
-    z1 = relay.add(y, c)
-    z2 = relay.add(z, z1)
-    z2 = relay.exp(z2)
-    mult_out = relay.Tuple([z2, conv, conv2])
-    return relay.Function([x, weight], mult_out)
+    return relay.Function([x,weight], conv)
 
 ###############################################################################
 # Optimize the Program
@@ -45,21 +54,19 @@ inferType = relay.transform.InferType()
 mod = inferType(mod)
 # Now we can apply constant folding on the module.
 # fold_const here is a callback that doesn't take any parameters.
-fastMath = relay.transform.Extend2DConv()
-#common_sub = relay.transform.EliminateCommonSubexpr()
+convExt = relay.transform.Extend2DConv()
+commonSub = relay.transform.EliminateCommonSubexpr()
+foldConst = relay.transform.FoldConstant()
 #fastMath = relay.transform.ToANormalForm()
 # Then, we can invoke the pass on the given module. Note that the constant
 # folding pass works at the function-level. That being said, each function in
 # the module will be applied with the optimization. Users don't need to iterate
 # through individual functions manually to apply this pass.
 #mod = common_sub(mod)
-mod = fastMath(mod)
-# We can see from the updated program that the constants are folded.
-#print(mod)
+mod = commonSub(mod)
+mod = convExt(mod)
+mod = commonSub(mod)
+print(mod)
 
-
-###############################################################################
-# More optimizations can be applied in the similar manner. For instance, we can
-# eliminate the common expressions that used by `z` and `z1`.
-#mod = relay.transform.EliminateCommonSubexpr()(mod)
-#print(mod)
+mod = foldConst(mod)
+print(mod)
